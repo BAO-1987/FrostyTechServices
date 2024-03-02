2ce8572d7fdd7c960f8a49b15173b1687a5e4414
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-03-02 21:38:15 +0200

    add request

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,14 +1,43 @@
 import './components/_burger';
 import './components/_slider';
 
-document.querySelectorAll('.request-btn').forEach(btn => {
+// const page = document.querySelector('.page');
+
+
+// document.querySelectorAll('.btn--sign').forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     document.querySelector('.formsapp-iframe.fullscreen').classList.add('is-open');
+//     page ? .classList.add('open');
+//   });
+// });
+
+// document.addEventListener('click', e => {
+//   if (e.target.className === 'formsapp-iframe fullscreen') {
+//     document.querySelector('.formsapp-iframe.fullscreen').classList.remove('is-open');
+//     page?.classList.remove('open');
+//   }
+// });
+
+const page = document.querySelector('.page');
+
+document.querySelectorAll('.btn--sign').forEach(btn => {
   btn.addEventListener('click', () => {
-    document.querySelector('#form-request').classList.add('open');
+    document.querySelector('.formsapp-iframe.fullscreen').classList.add('is-open');
+    if (page) {
+      page.classList.add('open');
+    }
   });
 });
 
 document.addEventListener('click', e => {
-  if (e.target.id === 'form-request') {
-    document.querySelector('#form-request').classList.remove('open');
+  const targetClasses = e.target.className.split(' ');
+  if (targetClasses.includes('formsapp-iframe') && targetClasses.includes('fullscreen')) {
+    document.querySelector('.formsapp-iframe.fullscreen').classList.remove('is-open');
+    if (page) {
+      page.classList.remove('open');
+    }
   }
 });
+
+
+
