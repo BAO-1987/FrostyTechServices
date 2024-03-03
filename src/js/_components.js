4db74d9792279ebc336b708e736d5af67b3b0cae
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-03-03 18:12:39 +0200

    fix after rewievs

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,43 +1,6 @@
 import './components/_burger';
 import './components/_slider';
 
-// const page = document.querySelector('.page');
-
-
-// document.querySelectorAll('.btn--sign').forEach(btn => {
-//   btn.addEventListener('click', () => {
-//     document.querySelector('.formsapp-iframe.fullscreen').classList.add('is-open');
-//     page ? .classList.add('open');
-//   });
-// });
-
-// document.addEventListener('click', e => {
-//   if (e.target.className === 'formsapp-iframe fullscreen') {
-//     document.querySelector('.formsapp-iframe.fullscreen').classList.remove('is-open');
-//     page?.classList.remove('open');
-//   }
-// });
-
-const page = document.querySelector('.page');
-
-document.querySelectorAll('.btn--sign').forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.querySelector('.formsapp-iframe.fullscreen').classList.add('is-open');
-    if (page) {
-      page.classList.add('open');
-    }
-  });
-});
-
-document.addEventListener('click', e => {
-  const targetClasses = e.target.className.split(' ');
-  if (targetClasses.includes('formsapp-iframe') && targetClasses.includes('fullscreen')) {
-    document.querySelector('.formsapp-iframe.fullscreen').classList.remove('is-open');
-    if (page) {
-      page.classList.remove('open');
-    }
-  }
-});
 
 
 
